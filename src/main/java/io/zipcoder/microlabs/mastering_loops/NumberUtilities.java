d87790b1e84b93f6d94f038e657576fc1065b15d
Alissa <alissa@Zipcoders-MBP-3.hsd1.de.comcast.net>
2022-03-03 00:29:02 -0500

    Most Utl test work

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,16 +3,34 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                result = result + i;
+            }
+
+
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 != 0) {
+                result = result + i;
+            }
+
+
+        }
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
+
+
         return null;
     }
 
@@ -22,15 +40,18 @@ public class NumberUtilities {
 
     public static String getRange(int start, int stop) {
         return null;
+
     }
 
 
     public static String getRange(int start, int stop, int step) {
+       // if ( start <= )
         return null;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
         return null;
+
     }
 }
