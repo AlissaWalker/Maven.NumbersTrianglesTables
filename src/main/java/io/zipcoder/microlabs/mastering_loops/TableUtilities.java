3a9865b9d75a4ba27d26e28e5ea1f8f56a1a85ac
Alissa <alissa@Zipcoders-MBP-3.hsd1.de.comcast.net>
2022-03-07 00:19:08 -0500

    Multiplication Test Done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,25 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(5);// set table size to 5 because test is 5 * 5
+
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(10); // set table size to 10 because test is 10 * 10
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result ="";
+        StringBuilder sb= new StringBuilder();
+        for(int i= 1; i <= tableSize; i++) {
+           for (int j =1; j<= tableSize; j++) {
+               sb.append(String.format("%3d |", i*j ));
+           }
+           sb.append("\n");
+           }
+        result= sb.toString() ;
+        return  result;
     }
 }
+
